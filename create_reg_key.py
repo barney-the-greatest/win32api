247ed483aee5bc2c7c7d0249110be2aa8ad800ac
Barney Noble <barneynoble@hotmail.co.uk>
2018-01-28 21:26:38 +0000

    adding forgotten extension, updating key value to all access and changing the type to be a variable for easier reuse.

--- create_reg_key.py
+++ create_reg_key.py
@@ -5,8 +5,8 @@ import _winreg
 reg_var = _winreg.HKEY_LOCAL_MACHINE
 string_var = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system"
 key_value = "LocalAccountTokenFilterPolicy"
-
+key_type = "_winreg.REG_DWORD"
 #open handle to the key, creates the key, gives the values to the key.
 x = _winreg.OpenKeyEx(reg_var, string_var, 0, _winreg.KEY_WRITE)
 y = _winreg.CreateKey(x, key_value, )
-y = _winreg.SetValueEx(y, key_value,_winreg.REG_DWORD,1,'1',)
+y = _winreg.SetValueEx(y, key_value,key_type,1,'1',)
